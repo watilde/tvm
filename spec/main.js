8f237976fbda1bef486bc1056354743783cb8e09
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-14 13:05:07 +0900

    Fix spec

--- spec/main.js
+++ spec/main.js
@@ -2,33 +2,33 @@
   'use strict';
   var define = require('./define');
   var assert = require('assert');
-  var tvm    = require('../tvm');
+  var tvm    = require('../lib/tvm');
 
   describe('tvm', function() {
     it('usage', function() {
-        assert(tvm.usage());
+        assert(tvm.usage(define));
     });
     it('list_known', function() {
-        assert(tvm.list_known());
+        assert(tvm.list_known(define));
     });
     it('install', function() {
-        assert(tvm.install('0.9.0'));
-        assert(tvm.install('0.9.5'));
+        assert(tvm.install('0.9.0', define));
+        assert(tvm.install('0.9.5', define));
     });
     it('use', function() {
-        assert(tvm.use('0.9.5'));
+        assert(tvm.use('0.9.5', define));
     });
     it('list', function() {
-        assert(tvm.list());
+        assert(tvm.list(define));
     });
     it('uninstall', function() {
-        assert(tvm.uninstall('0.9.0'));
+        assert(tvm.uninstall('0.9.0', define));
     });
     it('list', function() {
-        assert(tvm.list());
+        assert(tvm.list(define));
     });
     it('clean', function() {
-        assert(tvm.clean());
+        assert(tvm.clean(define));
     });
   });
 

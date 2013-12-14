f3d53bc5984544fc0b58468bbc0b4e3172b9c92b
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-15 02:29:10 +0900

    Add asynchronous spec test

--- spec/define.js
+++
@@ -1,7 +0,0 @@
-exports.dirname = {
-  typescript: __dirname + '/typescript/',
-  src: __dirname + '/src/',
-  current: __dirname + '/current'
-};
-
-exports.url =  'http://registry.npmjs.org/typescript/';

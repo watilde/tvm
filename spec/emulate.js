d937f4f0f0605e4d043575973fd253d22b380210
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-27 18:41:53 +0900

    Update line

--- spec/emulate.js
+++ spec/emulate.js
@@ -6,7 +6,7 @@
   describe('tvm', function() {
     this.timeout(5000);
     before(function(done) {
-      console.log('Emulating the behavior of using tvm.\n\n');
+      console.log('TEST BEHAVIOR\n\n');
       tvm.clean(done);
     });
 
